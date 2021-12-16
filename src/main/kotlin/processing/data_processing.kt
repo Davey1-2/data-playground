0acdc8fd584a649d17a3939e7a71cac97e6c0482
Davey1-2 <david.barta2014gmail.com>
2021-12-16 23:56:45 +0100

    replaced java things

--- src/main/kotlin/processing/data_processing.kt
+++ src/main/kotlin/processing/data_processing.kt
@@ -8,54 +8,50 @@ import java.util.stream.Collectors
 
 
 fun atLeastOneGradeA(student: Student): Boolean {
-    return student.grades.stream().anyMatch { grade: Grade -> grade.type == GradeType.A }
+    return student.grades.any { grade: Grade -> grade.type == GradeType.A }
 }
 
 
 fun getStudentAges(students: List<Student>): List<Int> {
-    return students.stream()
-        .map { obj: Student -> obj.age }
-        .collect(Collectors.toList())
+    return students.map { obj: Student -> obj.age }
 }
 
 fun getStudentsWithMinimumAge(students: List<Student>, minAge: Int): List<Student> {
-    return students.stream()
-        .filter { student: Student -> student.age <= minAge }
-        .collect(Collectors.toList())
+    return students.filter { student: Student -> student.age <= minAge }
+
 }
 
 
 // gender == Gender.MALE
 // or gender.name == "MALE"
 fun countMaleStudents(students: List<Student>): Int {
-    return students.stream()
-        .filter { student: Student -> student.gender == Gender.MALE }
-        .count().toInt()
+    return students.filter { student: Student -> student.gender == Gender.MALE }.count()
 }
 
 
 // gender == Gender.FEMALE
 // or gender.name == "FEMALE"
 fun avgAgeOfFemaleStudent(students: List<Student>): Double {
-    return students.stream()
+    return students.asSequence()
         .filter { student: Student -> student.gender == Gender.FEMALE }
-        .mapToInt { obj: Student -> obj.age }
+        .map { obj: Student -> obj.age }
         .average()
-        .orElseThrow()
 }
 
 fun getProductOfStudentAges(students: List<Student>): Int {
-    return students.stream().mapToInt { obj: Student -> obj.age }
+    return students.asSequence()
+        .map { obj: Student -> obj.age }
         .reduce { studentFirst: Int, studentSecond: Int -> studentFirst * studentSecond }
-        .orElseThrow()
+
 }
 
 // ignore F Grades
 fun productOfStudentGrades(student: Student): Int {
-    return student.grades.stream().mapToInt { grade: Grade -> grade.type.value }
+    return student.grades.asSequence()
+        .map { grade: Grade -> grade.type.value }
         .filter { grade: Int -> grade != GradeType.F.value }
         .reduce { grade1: Int, grade2: Int -> grade1 * grade2 }
-        .orElseThrow()
+
 }
 
 // region BONUS
